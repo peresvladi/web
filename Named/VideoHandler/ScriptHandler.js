8d49ef4cde25326473fcab159ccaea1edcfad85c
peresvladi <peresvladi@gmail.com>
2023-12-21 09:34:10 +0300

    меню работает, вроде

--- Named/VideoHandler/ScriptHandler.js
+++ Named/VideoHandler/ScriptHandler.js
@@ -379,69 +379,6 @@ document.getElementById("top").onchange = function () {
 };
 
 
-
-
-
-
-
-
-
-
-
-// document.write(`<select name="listNamberOne" id = "listOne">Choose a list:</label>`);
-
-// document.getElementById("listOne").onchange = function () {
-//   let sheet = document.getElementById("listOne").value;
-
-//   if (sheet == "Добавить") {
-//     //callsTheInputfile2(e = "v");
-//     callsTheInputfile1();
-//   }
-//   function callsTheInputfile1() {
-//     return document.getElementById('inputfile1').click();
-// };
-// document.write(`<option onclick= 'inputFile()' value='Пусто'>'Пусто'</option>`);
-// document.write(`<option onclick= 'inputFile()' value='Добавить'>'Добавить'</option>`);
-// if (window.localStorage.getItem("0" + name) === null) {
-//  alert (name + ' - в localStorage не вставлен, ОШИБКА!')};
-//  for (let i = 0; i < window.localStorage.length; i++) { 
-//  if(window.localStorage.key(i).charAt(0)==="0"){
-//  let  nameВutton = window.localStorage.key(i);
-//  let  valueВutton = window.localStorage.getItem(nameВutton);
-//  document.write(`<option value='${valueВutton}'>'${nameВutton}'</option>`);
-// }
-// }
-
-
-
-
-//  for (let i = 0; i < window.localStorage.length; i++) {
-//     let keyFragment = i + name;
-//     let fragment = (window.localStorage.getItem(keyFragment)).split(",");
-//     let nameInputButton = fragment[0] + "&" + fragment[1];
-//     let meFragment = fragment[2];
-//     let nameFragment = i + "-" + meFragment.replace(/\s/g, "&nbsp");
-//     document.write(`<option value='${nameFragment}'>'${nameFragment}'</option>`);
-//   }
-
-
-
-
-
-
-
-// function httpGetAsync(theUrl, callback) {
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.onreadystatechange = function () {
-//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-//             callback(xmlHttp.response);
-//     }
-//     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-//     xmlHttp.send(null);
-// }
-
-
-
 document.onkeydown = go_key;
 function go_key(event) {
     if (!event) { event = window.event; }
