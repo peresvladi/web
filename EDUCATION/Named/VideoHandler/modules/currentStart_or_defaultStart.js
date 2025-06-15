bffb322d5927f04e2aa6a6f2514715135422c365
vladiperes <peresvadi@mail.ru>
2025-06-15 22:21:39 +0300

    .\modules\currentStart_or_defaultStart.js .\Handler_copy.html .\ScriptHandler_copy.js update 15-06-2025

--- EDUCATION/Named/VideoHandler/modules/currentStart_or_defaultStart.js
+++ EDUCATION/Named/VideoHandler/modules/currentStart_or_defaultStart.js
@@ -1,23 +1,14 @@
-const defaultStart_or_currentStart = (incomingArray) => { //При выборе All в меню List предлагает выбрать старт отрезка с начала по умолчанию - 0 (0 указанного в записи ALL и присвоенного уже переменной sss), или с текущего значения старт (sss т.е. start присваемого от текущего места положения воспроизведения текущей записи)  
-    let ss;
-    if (incomingArray[3] === "All") {
-        let TrueFalse = confirm("Для продолжения работы в режиме All c текущего отрезка выберите ОК, для перехода в начальное положение текущей записи, ВЫБЕРИТЕ - CANCEL, или нажмите Esc на клавиатуре");
-        if (TrueFalse === true && incomingArray[3] === "All") { ss = Math.floor(vid.currentTime); }
-        if (TrueFalse === false && incomingArray[3] === "All") { ss = 0 };
-    } else { ss = incomingArray[1]; }
-    return ss;
-}
-const defaultStart_or_currentStart = (incomingArray) => { //При выборе All в меню List предлагает выбрать старт отрезка с начала по умолчанию - 0 (0 указанного в записи ALL и присвоенного уже переменной sss), или с текущего значения старт (sss т.е. start присваемого от текущего места положения воспроизведения текущей записи)
-    setPause()
-    let ss;
-    if (incomingArray[3] === "All") {
-        let TrueFalse = confirm("Для продолжения работы в режиме All c текущего отрезка выберите ОК, для перехода в начальное положение текущей записи, ВЫБЕРИТЕ - CANCEL, или нажмите Esc на клавиатуре");
-        if (TrueFalse === true && incomingArray[3] === "All") { ss = Math.floor(vid.currentTime); }
-        if (TrueFalse === false && incomingArray[3] === "All") { ss = 0 };
-    } else { ss = incomingArray[1]; }
-    return ss;
-}
+const defaultStart_or_currentStart = (incomingArray) =>{ //При выборе All в меню List предлагает выбрать старт отрезка с начала по умолчанию - 0 (0 указанного в записи ALL и присвоенного уже переменной sss), или с текущего значения старт (sss т.е. start присваемого от текущего места положения воспроизведения текущей записи)
+setPause()  
+let ss;
+if (incomingArray[3] === "All") {
+let TrueFalse = confirm("Для продолжения работы в режиме All c текущего отрезка выберите ОК, для перехода в начальное положение текущей записи, ВЫБЕРИТЕ - CANCEL, или нажмите Esc на клавиатуре");
+if (TrueFalse === true && incomingArray[3] === "All"){ss = Math.floor(vid.currentTime);}
+if(TrueFalse === false && incomingArray[3] === "All"){ss = 0};
+} else {ss = incomingArray[1];}
+return ss;
+}  
 
 function setPause() {
-    vid.pause();
+vid.pause();
 }
