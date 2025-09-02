16014534112c975e9e876dc09346a7492cee0692
peresvladi <peresvladi@gmail.com>
2025-09-02 15:35:11 +0300

    update: .\modules\CarrentDateTime\cM.js .\modules\createValueTextarea.js .\modules\deletingByKey.js .\modules\descriptionOfTheContent.txt .\modules\meny2.js

--- EDUCATION/Named/VideoHandler/modules/CarrentDateTime/cM.js
+++ EDUCATION/Named/VideoHandler/modules/CarrentDateTime/cM.js
@@ -1,5 +1,5 @@
 const cM =()=>{
-const carrentMonth = new Date().getMonth().toString();
+const carrentMonth = (new Date().getMonth() + 1).toString();
 let binaryFormatCarrentMonth;
 if(carrentMonth.length < 2){
 binaryFormatCarrentMonth = '0' + carrentMonth;
@@ -7,3 +7,4 @@ binaryFormatCarrentMonth = '0' + carrentMonth;
 {binaryFormatCarrentMonth = carrentMonth};
 return binaryFormatCarrentMonth;
 }
+// прочитал в нете, что месяцы(именно месяцы другие эл.даты не так) начинают считать с 0, т.е. по факту 0 это 1, 1 это по фату 2 и т.д., в связи с этим во 2 сроке добавляют + 1 (30-08.24)
--- EDUCATION/Named/VideoHandler/modules/meny2.js
+++ EDUCATION/Named/VideoHandler/modules/meny2.js
@@ -5,6 +5,9 @@ function meny2(a = meny1()) {
     document.write(`<select name='${nameSelectedMeny1}' id="topic">`); // записывает тег в подменю веб документа
     let i = 1; // создает переменную со значением для использование ниже
     //alert(window.localStorage.getItem(selectedNameMeny1.replace("0", i)));
+
+    let nameValueВuty = "1~ 0~0~All~0~0~0~0~0~0~0"; //020925
+
     while (getItemFunction(selectedNameMeny1.replace("0", i)) !== null) {
 
         let nameВutton = selectedNameMeny1.replace("0", i);
@@ -13,23 +16,28 @@ function meny2(a = meny1()) {
             var nameValueВut = i + valueSeparator() + whitespace + getItemFunction(nameВutton);
         } else {
             nameValueВut = "*" + i + valueSeparator() + whitespace + window.localStorage.getItem(nameВutton).slice(1); // Присваиваем значение пункта меню имеющего звезвдочку
-            var nameValueВuty = nameValueВut;// Присваиваем значение пункта меню имеющего звезвдочку переменной nameValueВuty (nameValueВuty затем будет использована для вывода в первую строку меню пункта со звездочкой, а также для присовоения текстовому полю комментария - текста комментария из текстового значения пункта меню со звездочкой)
+            nameValueВuty = nameValueВut;// Присваиваем значение пункта меню имеющего звезвдочку переменной nameValueВuty (nameValueВuty затем будет использована для вывода в первую строку меню пункта со звездочкой, а также для присовоения текстовому полю комментария - текста комментария из текстового значения пункта меню со звездочкой)
         }
         i++;
         document.write(`<option value='${nameValueВut}'>'${nameValueВut.split(valueSeparator())[0].replace("*", "  *  ") + ") " + nameValueВut.split(valueSeparator())[3]}'</option>`);
     }
     document.write(`</select>`);
+    if (nameValueВuty === '1~ 0~0~All~0~0~0~0~0~0~0') { setItemFunction('1' + selectedNameMeny1, '*0~0~All~0~0~0~0~0~0~0') };//020925
     document.getElementById("topic").value = nameValueВuty;
     //let createValTextarea = nameValueВuty.split(valueSeparator()).slice(-1);
     let createValTextarea = createValueTextarea(nameValueВuty, valueSeparator(), -1);
-    //alert("пришло в meny2.js >>> " + createValTextarea);
+    // alert("пришло в meny2.js >>> " + createValTextarea);
 
 
+    // <button id="testCode" style="display:block">Пли..Тест</button>
+    document.write(`<button id="hideButton">.   .   .</button><div id="myElements" style = "display:none";><textarea id ="valueTextarea" name = "comment" rows="5" cols=90% minlength="2" maxlength="255" wrap="physical">'${createValTextarea}'</textarea><div class="groupOfControlButtons"><button id="saveButtonTextarea" style = "display:block";>сохранить комментарий</button><button id="editMenuItem">редактировать меню</button><button id="saveMenuItem" style = "display:none";>сохранить меню</button></div></div> <a href="#" onclick="keyByDeleting(event)" style = "display: none" id = "myLink">удалить запись меню с..по..</a>`);
 
-    document.write(`<button id="testCode" style="display:block">Пли..Тест</button><button id="hideButton">.   .   .</button><div id="myElements" style = "display:none";><textarea id ="valueTextarea" name = "comment" rows="5" cols=90% minlength="2" maxlength="255" wrap="physical">'${createValTextarea}'</textarea><div class="groupOfControlButtons"><button id="saveButtonTextarea" style = "display:block";>сохранить комментарий</button><button id="editMenuItem">редактировать меню</button><button id="saveMenuItem" style = "display:none";>сохранить меню</button></div></div> `); // (14.06.2025) эти строки кода вставляют текстовый комментарий либо символы "~0" указывающие на его отсутсвие из строки значения localStorage (комментарий расположен последним в строке и извлекается поэтому: nameValueВuty.split(valueSeparator()).slice(-1))
+    // (14.06.2025) эти строки кода вставляют текстовый комментарий либо символы "~0" указывающие на его отсутсвие из строки значения localStorage (комментарий расположен последним в строке и извлекается поэтому: nameValueВuty.split(valueSeparator()).slice(-1))
 
-    const testCode = document.getElementById('testCode');
-    testCode.addEventListener('click', keyByDeleting);
+    //28.08.25 к вышезаписаной загрузке эелементов ( т.е. к document.write....) дописал в конце (<a href="#" onclick="keyByDeleting(event)" style = "display: none" id = "myLink">удалить запись меню с..по..</a>). Это невидимая ссылка которую делает видимой ниже расположенный код (hideButton.addEventListener('click', () => {.......), который одновременно дописал (так, в конце if добавил: myLink.style.display = 'none' ; в конце else добавил: myLink.style.display = 'block';). Теперь после нажатия кнопки 'hideButton' ссылка становиться видимой, после чего при нажатии на нее своим кодом (onclick="keyByDeleting(event)") запускает функцию keyByDeleting(event), которая выполняет алгоритм удаления записей из localStorage хранилища браузера хром.
+
+    // const testCode = document.getElementById('testCode');
+    // testCode.addEventListener('click', keyByDeleting);
 
     const hideButton = document.getElementById('hideButton');
     const myElements = document.getElementById('myElements');
@@ -41,8 +49,13 @@ function meny2(a = meny1()) {
     hideButton.addEventListener('click', () => {
         if (myElements.style.display === 'block') {
             myElements.style.display = 'none';
+            myLink.style.display = 'none';
+
         }
-        else { myElements.style.display = 'block'; }
+        else {
+            myElements.style.display = 'block';
+            myLink.style.display = 'block';
+        }
 
     });
     saveButtonTextarea.addEventListener('click', saveButtonTextareaFunction);
@@ -72,3 +85,5 @@ function meny2(a = meny1()) {
     })
 
 }
+
+//020925 - изменен код для борьбы с ошибкой - "потеря звездочки (*)" При потере звездочки (например при удалении записей меню (в том числе и записи со звездочкой) из localStorag для их перезаписи (например с целью обновления) ранее переменная nameValueВuty оставалась без инициализации (в состоянии - undefind), что влекло ошибки, например при попытки использовать в дальнейшем коде nameValueВuty(которая осталась без инициализации - undefind). Поэтому для исправления ошибки переменной nameValueВuty сразу при ее объявлении было присвоено значение (Вот-так: let nameValueВuty = "1~ 0~0~All~0~0~0~0~0~0~0";) А в случае обнаружения оператором if (Вот этим: if (nameValueВuty === '1~ 0~0~All~0~0~0~0~0~0~0') { setItemFunction('1' + selectedNameMeny1, '*0~0~All~0~0~0~0~0~0~0') })что значение переменной осталось без изменениий (либо было заменено в коде на аналогичное значение без взездочки), тогда указанный код после if вносит шаблонное значение со звездочкой в localStorage ('*0~0~All~0~0~0~0~0~0~0') для дальнейшего его использования как выбранного после перезагрузки в обычном режиме. И, что-бы обойти ошибку в данный момент предварительно вызывает в меню записанный пункт ('1~ 0~0~All~0~0~0~0~0~0~0' т.е. без звездочки), который в дальнейшем при перезакрузке скачивается из localStorage в пункт меню со звездочкой, исправляя таким образом ошибку "потери звездочки" после чего код должен быть исправлен и работать штатно.
